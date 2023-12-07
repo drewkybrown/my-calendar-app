1c08f3c5629c60d929367a95761071c06ac355d2
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-07 15:59:38 -0500

    create calendar function

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -16,3 +16,25 @@ const daysOfWeek = [
   "Saturday",
 ]; // Array of days of the week
 const dates = Array.from({ length: 28 }, (_, i) => i + 1); // Array of dates in the month
+// Array.from() creates a new, shallow-copied Array instance from an array-like or iterable object.
+// { length: 28 } This is the array-like object. It's an object with a single property `length` set to 28.
+// 1. This tells `Array.from` to create an array with 28 elements.
+// Array.from` is a map function, which takes two parameters: `(_, i)`.
+// The underscore `_` is a convention in JavaScript to indicate that the parameter is not being used.
+// (in this case, the current element, which doesn't exist yet). `i` represents the index of the current element.
+
+// export default function Calendar
+
+export default function Calendar() {
+  const [selectedCategory, setSelectedCategory] = useState("null"); // React hook
+  // `selectedCategory` is the current value of the dropdown menu.
+  const [cellColors, setCellColors] = useState({}); // React hook
+  // `cellColors` is an object that maps dates to colors.
+
+  const handleCellClick = (date) => {
+    if (selectedCategory) {
+      setCellColors({ ...cellColors, [date]: selectedCategory });
+      // `handleCellClick` is a function that takes a date as a parameter.
+    }
+  };
+}
